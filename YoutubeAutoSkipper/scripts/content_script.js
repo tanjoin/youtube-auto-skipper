703a60913f66e16d146be5fe5c2e6021f4a47c8b
tanjoin <tanjoin@users.noreply.github.com>
2018-09-17 02:44:22 +0900

    ずっと動かす必要がなくなった

--- YoutubeAutoSkipper/scripts/content_script.js
+++ YoutubeAutoSkipper/scripts/content_script.js
@@ -1,17 +1,28 @@
 ((global) => {
   'use strict';
   chrome.extension.sendRequest({}, (res) => {});
-
-  setInterval(() => {
-    const skip = document.querySelector('#movie_player > div.video-ads > div > div > div.videoAdUiSkipContainer.html5-stop-propagation > button');
-    if (skip) {
-      skip.click();
-      console.log("YoutubeAutoSkipper skip ads!");
-    }
-    const close = document.querySelector('div.close-padding').click();
-    if (close) {
-      close.click();
-      console.log("YoutubeAutoSkipper closes ads!");
-    }
-  }, 1000);
+  setup();
 })(this.self || global);
+
+function setup() {
+  var target = document.querySelector('#movie_player > div.video-ads > div.ad-container');
+  if (!target) {
+    window.setTimeout(setup, 500);
+    return;
+  }
+  var observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutationRecord) => {
+      const skip = document.querySelector('#movie_player > div.video-ads > div > div > div.videoAdUiSkipContainer.html5-stop-propagation > button');
+      if (skip) {
+        skip.click();
+        console.log("YoutubeAutoSkipper skip ads!");
+      }
+      const close = document.querySelector('div.close-padding');
+      if (close) {
+        close.click();
+        console.log("YoutubeAutoSkipper closes ads!");
+      }
+    });
+  });
+  observer.observe(target, { childList: true });
+};

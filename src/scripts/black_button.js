2cb08b08a3a64251db8d63b522d4ba17dd41d929
tanjoin <tanjoin@users.noreply.github.com>
2021-06-10 13:14:42 +0900

    タイトルを格納する

--- src/scripts/black_button.js
+++ src/scripts/black_button.js
@@ -6,6 +6,44 @@
 
 var black_button_count = 0;
 
+function addDarkButton(ygvr) {
+  var thumbnail = ygvr.querySelector('#thumbnail');
+  var overlay = ygvr.querySelector('#overlays');
+  var dismiss = document.createElement('button');
+  dismiss.className = "tj-kurakusuru";
+  dismiss.width = "30px";
+  dismiss.height = "30px";
+  dismiss.textContent = "× 暗くする";
+  dismiss.style.fontSize = "10px";
+  dismiss.style.position = "absolute";
+  dismiss.style.bottom = 0;
+  dismiss.style.backgroundColor = "black";
+  dismiss.style.color = "white";
+  dismiss.addEventListener("click", (e) => { 
+    e.preventDefault();
+    e.stopPropagation();
+    ygvr.style.opacity = "0.1";
+    var a = ygvr.querySelector('a');
+    console.log(a);
+    if (a) {
+      var titleView = ygvr.querySelector('#video-title');
+      var title = true;
+      if (titleView) {
+        title = titleView.textContent;
+      }
+      localStorage.setItem('tj::' + a.href.split('&')[0].split('=')[1], title);
+    }
+    var bar = document.createElement('ytd-thumbnail-overlay-resume-playback-renderer');
+    bar.className = "style-scope ytd-thumbnail";
+    var progress = document.createElement('div');
+    progress.id = "progress";
+    progress.className = "style-scope ytd-thumbnail-overlay-resume-playback-renderer";
+    bar.appendChild(progress);
+    overlay.appendChild(bar);
+  });
+  thumbnail.appendChild(dismiss);
+}
+
 function setup() {
   var target = document.querySelector('body');
   if (!target) {
@@ -19,35 +57,7 @@ function setup() {
         return !e.querySelector(".tj-kurakusuru");
       })
       .forEach((ygvr) => {
-        var thumbnail = ygvr.querySelector('#thumbnail');
-        var overlay = ygvr.querySelector('#overlays');
-        var dismiss = document.createElement('button');
-        dismiss.className = "tj-kurakusuru";
-        dismiss.width = "30px";
-        dismiss.height = "30px";
-        dismiss.textContent = "× 暗くする";
-        dismiss.style.fontSize = "10px";
-        dismiss.style.position = "absolute";
-        dismiss.style.bottom = 0;
-        dismiss.style.backgroundColor = "black";
-        dismiss.style.color = "white";
-        dismiss.addEventListener("click", (e) => { 
-          e.preventDefault();
-          e.stopPropagation();
-          ygvr.style.opacity = "0.1";
-          var a = ygvr.querySelector('a');
-          if (a) {
-            localStorage.setItem('tj::' + a.href.split('&')[0].split('=')[1]);
-          }
-          var bar = document.createElement('ytd-thumbnail-overlay-resume-playback-renderer');
-          bar.className = "style-scope ytd-thumbnail";
-          var progress = document.createElement('div');
-          progress.id = "progress";
-          progress.className = "style-scope ytd-thumbnail-overlay-resume-playback-renderer";
-          bar.appendChild(progress);
-          overlay.appendChild(bar);
-        });
-        thumbnail.appendChild(dismiss);
+        addDarkButton(ygvr);
       });
     }
     black_button_count++;

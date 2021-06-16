1be3a7efd9b51d815b5a672e4fc7bb30ce867ba3
tanjoin <tanjoin@users.noreply.github.com>
2021-06-16 22:32:18 +0900

    走査は①回

--- src/scripts/viewed_black.js
+++ src/scripts/viewed_black.js
@@ -14,9 +14,13 @@ function setup() {
   }
   var observer = new MutationObserver((mutations) => {
     if (viewed_black_count % 5 === 0) {
-      [...document.querySelectorAll('ytd-grid-video-renderer')].filter((e) => e.querySelector('#progress')).forEach((e) => e.style.opacity = "0.1");
-      [...document.querySelectorAll('ytd-grid-video-renderer')].filter((e) => !e.querySelector('#progress')).forEach((e) => e.style.opacity = "1.0");
-      [...document.querySelectorAll('ytd-grid-video-renderer')].filter((e) => localStorage.getItem("tj::" + e.querySelector('a').href.split('&')[0].split('=')[1])).forEach((e) => e.style.opacity = 0.1);
+      [...document.querySelectorAll('ytd-grid-video-renderer')].forEach((e) => {
+        if (e.querySelector('#progress') || localStorage.getItem("tj::" + e.querySelector('a').href.split('&')[0].split('=')[1])) {
+          e.style.opacity = "0.1"
+        } else {
+          e.style.opacity = "1.0";
+        }
+      });
     }
     viewed_black_count++;
   });

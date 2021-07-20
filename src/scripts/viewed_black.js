24288ae23eedc9b19c2efe972ef6706369362602
tanjoin <tanjoin@users.noreply.github.com>
2021-07-21 02:59:09 +0900

    透明化の濃度を変える

--- src/scripts/viewed_black.js
+++ src/scripts/viewed_black.js
@@ -16,7 +16,9 @@ function setup() {
     if (viewed_black_count % 5 === 0) {
       [...document.querySelectorAll('ytd-grid-video-renderer')]
       .forEach((e) => {
-        if (e.querySelector('#progress') || localStorage.getItem("tj::" + e.querySelector('a').href.split('&')[0].split('=')[1])) {
+        if (e.querySelector('#progress')) {
+          e.style.opacity = "0.3";
+        } else if (localStorage.getItem("tj::" + e.querySelector('a').href.split('&')[0].split('=')[1])) {
           e.style.opacity = "0.1";
         } else {
           e.style.opacity = "1.0";

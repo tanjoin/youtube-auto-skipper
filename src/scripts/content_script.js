a448a4fb0f6706633aea188e1dd698db5f227746
tanjoin <tanjoin@users.noreply.github.com>
2024-11-04 10:09:56 +0900

    class 化

--- src/scripts/content_script.js
+++ src/scripts/content_script.js
@@ -1,211 +1,389 @@
-((global) => {
-  "use strict";
-  setup();
-})(this.self || global);
-
-var ad_count = 0;
-let is_skipping = 0;
-var oldUrl = '';
-
-window.addEventListener("urlChange", () => {
-  console.log("urlChange");
-  if (location.href.includes("/watch?v=")) {
-    skip();
-    close();
-  }
-});
-
-function setup() {
-  var target = document.body;
-  if (!target) {
-    window.setTimeout(setup, 500);
-    return;
-  }
-  var observer = new MutationObserver((mutations) => {
-    skip();
-    close();
-    if (ad_count % 10 === 0) {
-      viewedBlack();
-    }
-    if (oldUrl !== location.href) {
-      oldUrl = location.href;
-      window.dispatchEvent(new Event("urlChange"));
-    }
-  });
-  observer.observe(target, {
-    childList: true,
-    subtree: true,
-  });
-}
-
-function runtimeAdSkip(x, y, callback) {
-  chrome.runtime.sendMessage({ action: "skip", x, y }, callback);
-}
-
-function skip() {
-  if (is_skipping > 0) {
-    console.log("is skipping");
-    if (is_skipping > 10) {
-      setTimeout(() => skip(), 5000);
-      is_skipping = 0;
+class ViewedBlackSwitchLargeButton {
+  constructor(e) {
+    if (e) {
+      this.e = e;
     } else {
-      is_skipping++;
+      this.e = document.createElement("button");
+      this.e.style.border = "0";
+      this.e.style.color = "white";
+      this.e.style.marginRight = "6px";
+      this.e.style.width = "100%";
+      this.e.id = "viewed_black_main";
     }
-    return;
   }
-  const button = document.querySelector(".ytp-skip-ad-button, .ytp-ad-skip-button-modern");
-  const previewArea = document.querySelector(".ytp-preview-ad");
-  const timeDuration = document.querySelector(".ytp-time-duration");
-  if (!button && previewArea) {
-    let seconds = parseInt(
-      timeDuration?.textContent?.split(":")[0] * 60 +
-        timeDuration?.textContent?.split(":")[1]
+
+  toElement() {
+    return this.e;
+  }
+
+  apply(textContent, backgroundColor, onClick) {
+    this.e.textContent = textContent;
+    this.e.style.backgroundColor = backgroundColor;
+    this.e.addEventListener("click", onClick);
+  }
+
+  applyAlready(clickHandler) {
+    this.apply(`× 視聴済み（取得済み）`, "#6200EE", () => {
+      this.e.remove();
+      clickHandler();
+    });
+    return this;
+  }
+
+  applyNotWatched(clickHandler) {
+    this.apply(`+ 未視聴（未取得）`, "#018786", () => {
+      this.e.remove();
+      clickHandler();
+    });
+    return this;
+  }
+};
+
+class DeleteShortAreaController {
+  constructor() {
+    this.shortAreaCount = 0;
+  }
+
+  onLoad() {
+    window.addEventListener("shortAreaCountChange", this.deleteShortArea.bind(this));
+  }
+
+  observe() {
+    if (this.shortAreaCount !== this.getShortArea().length) {
+      this.shortAreaCount = this.getShortArea().length;
+      window.dispatchEvent(new Event("shortAreaCountChange"));
+    }
+  }
+
+
+  // ショート動画削除
+  deleteShortArea() {
+    this.getShortArea()
+      .filter((d) => !d.innerText.includes("新しい順"))
+      .forEach((d) => (d.style.display = "none"));
+  }
+
+  getShortArea() {
+    return [
+      ...document.querySelectorAll("div#contents ytd-rich-section-renderer"),
+    ];
+  }
+};
+
+class DismissAdController {
+  constructor() {
+  }
+
+  onLoad() {
+    window.addEventListener("showAd", this.performSkipAction.bind(this));
+  }
+
+  observe() {
+    if (this.getAdInterruptingElement()) {
+      window.dispatchEvent(new Event("showAd"));
+    }
+  }
+
+  urlChange() {
+    this.performSkipAction();
+  }
+
+  // Getter & Setter
+
+  getAdInterruptingElement() {
+    return document.querySelector(".ad-interrupting");
+  }
+
+  getSkipButton() {
+    return document.querySelector(
+      ".ytp-skip-ad-button, .ytp-ad-skip-button-modern"
     );
-    if (seconds > 10) {
-      let player = document.getElementById("movie_player");
-      if (player && player.getCurrentTime) {
-        let currentTime = player?.getCurrentTime();
-        if (currentTime) {
-          let t = parseInt(currentTime);
-          if (t) {
-            if (location.href.includes("/watch?v=")) {
-              location.href = location.href + "&t=" + t + "s";
-            }
-          }
-        }
-      } else {
-        if (location.href.includes("/watch?v=")) {
-          location.reload();
-        }
-      }
+  }
+
+  getTimeDuration() {
+    return document.querySelector(".ytp-time-duration");
+  }
+
+  getTimeCurrent() {
+    return document.querySelector(
+      "#movie_player:not(.ad-interrupting) .ytp-time-current"
+    );
+  }
+
+  // Boolean Methods
+
+  isVideoPage() {
+    return location.href.includes("/watch?v=");
+  }
+
+  // Methods
+
+  checkCurrentTime() {
+    if (!this.isVideoPage()) {
+      this.currentTime = 0;
       return;
     }
+    const timeCurrent = this.getTimeCurrent();
+    if (timeCurrent) {
+      this.currentTime = 0;
+      return;
+    }
+    let seconds = parseInt(
+      timeCurrent?.textContent?.split(":")[0] * 60 +
+        timeCurrent?.textContent?.split(":")[1]
+    );
+    this.currentTime = seconds;
   }
-  if (button) {
-    let is_fullscreen = (document.fullscreenElement != null);
-    is_skipping = 1;
+
+  runtimeAdSkip(x, y, callback) {
+    chrome.runtime.sendMessage({ action: "skip", x, y }, callback);
+  }
+
+  performSkip(button) {
+    let isFullscreen = document.fullscreenElement != null;
     let oX = button.getBoundingClientRect().x;
     let oY = button.getBoundingClientRect().y;
     if (oX <= 0 || oY <= 0) {
       is_skipping = 0;
+      setTimeout(this.performSkipAction.bind(this), 5000);
       return;
     }
-    runtimeAdSkip(oX, oY, (response) => {
-      console.log("youtubeAutoSkipper skip ads!");
-      is_skipping = 0;
+    this.runtimeAdSkip(oX, oY, (response) => {
       setTimeout(() => {
         document.querySelector("video").play();
-        if (is_fullscreen && document.fullscreenElement == null) {
+        if (isFullscreen && document.fullscreenElement == null) {
           document.documentElement.requestFullscreen();
         }
       }, 1000);
     });
   }
-}
 
-function close() {
-  const div = document.querySelector(
-    ".ytp-ad-overlay-close-container > button"
-  );
-  if (div) {
-    div.click();
-    console.log("YoutubeAutoSkipper closes ads!");
-  }
-}
-
-let nowUrl = undefined;
-function viewedBlack() {
-  if (nowUrl == location.href && document.getElementById("viewed_black_main")) {
-    return;
-  }
-  if (document.querySelector("#viewed_black_main")) {
-    document.querySelector("#viewed_black_main").remove();
-  }
-  let area = document.querySelector("#below");
-  if (!area) {
-    return;
-  }
-  area.removeAttribute("is-empty");
-  nowUrl = location.href;
-  if (location.href.split("&")[0].split("=")[1]) {
-    var id = location.href.split("&")[0].split("=")[1];
-    if (!localStorage.getItem("tj::" + id)) {
-      // blacked
-      console.log("viewedBlack : " + localStorage.getItem("tj::" + id));
-      let button = document.createElement("button");
-      button.textContent = "+ 未視聴（未取得）";
-      button.style.border = "0";
-      button.style.color = "white";
-      button.style.marginRight = "6px";
-      button.style.backgroundColor = "#018786";
-      // button.style.borderRadius = "10%";
-      button.style.width = "100%";
-      button.id = "viewed_black_main";
-      button.addEventListener("click", () => {
-        localStorage.setItem("tj::" + id, document.title);
-        button.remove();
-        nowUrl = undefined;
-        viewedBlack();
-      });
-      area.prepend(button);
-    } else {
-      console.log("viewedBlack : " + localStorage.getItem("tj::" + id));
-      let button = document.createElement("button");
-      button.textContent = "× 視聴済み（取得済み）";
-      button.style.border = "0";
-      button.style.color = "white";
-      button.style.marginRight = "6px";
-      button.style.backgroundColor = "#6200EE";
-      // button.style.borderRadius = "10%";
-      button.style.width = "100%";
-      button.id = "viewed_black_main";
-      button.addEventListener("click", () => {
-        localStorage.removeItem("tj::" + id, document.title);
-        button.remove();
-        nowUrl = undefined;
-        viewedBlack();
-      });
-      area.prepend(button);
+  performSkipAction() {
+    const button = this.getSkipButton();
+    if (button) {
+      button.style.display = "";
+      this.performSkip(button);
+      return;
     }
-    return;
-  }
-  if (location.href.split("&")[0].split("shorts/")[1]) {
-    var id = location.href.split("&")[0].split("shorts/")[1];
-    if (!localStorage.getItem("tj::" + id)) {
-      // short blacked
-      let button = document.createElement("button");
-      button.textContent = "+ 未視聴（未取得）";
-      button.style.border = "0";
-      button.style.color = "white";
-      button.style.marginRight = "6px";
-      button.style.backgroundColor = "#018786";
-      // button.style.borderRadius = "10%";
-      button.style.width = "100%";
-      button.id = "viewed_black_main";
-      button.addEventListener("click", () => {
-        localStorage.setItem("tj::" + id, document.title);
-        button.remove();
-        nowUrl = undefined;
-        viewedBlack();
-      });
-      area.prepend(button);
-    } else {
-      let button = document.createElement("button");
-      button.textContent = "× 視聴済み（取得済み）";
-      button.style.border = "0";
-      button.style.color = "white";
-      button.style.marginRight = "6px";
-      button.style.backgroundColor = "#6200EE";
-      // button.style.borderRadius = "10%";
-      button.style.width = "100%";
-      button.id = "viewed_black_main";
-      button.addEventListener("click", () => {
-        localStorage.setItem("tj::" + id, document.title);
-        button.remove();
-        nowUrl = undefined;
-        viewedBlack();
-      });
-      area.prepend(button);
+
+    const adInterruptingElement = this.getAdInterruptingElement();
+    const timeDuration = this.getTimeDuration();
+    if (adInterruptingElement && timeDuration) {
+      let seconds = parseInt(
+        timeDuration?.textContent?.split(":")[0] * 60 +
+          timeDuration?.textContent?.split(":")[1]
+      );
+      let isFullscreen = document.fullscreenElement != null;
+      if (seconds > 10) {
+        this.checkCurrentTime();
+        if (this.currentTime > 0) {
+          let params = new URLSearchParams(location.search);
+          params.set("t", this.currentTime);
+          if (isFullscreen) {
+            params.set("fullscreen", true);
+          }
+          location.search = params.toString();
+        } else {
+          let params = new URLSearchParams(location.search);
+          if (isFullscreen) {
+            params.set("fullscreen", true);
+          }
+          location.search = params.toString();
+          location.reload();
+        }
+      }
     }
   }
+};
+
+class ShowViewedBlackLargeButtonController {
+  constructor() {
+    this.currentBelow = null;
+  }
+
+  onLoad() {
+    window.addEventListener("showBelow", this.viewedBlack.bind(this));
+  }
+  
+  observe() {
+    let below = this.getBelow();
+    if (below && below !== this.currentBelow) {
+      this.currentBelow = this.getBelow();
+      window.dispatchEvent(new Event("showBelow"));
+    }
+  }
+
+  urlChange() {
+    this.viewedBlack();
+  }
+
+  // Getter & Setter
+
+  getBelow() {
+    return document.getElementById("below");
+  }
+
+  getViewedBlackMain() {
+    return document.getElementById("viewed_black_main");
+  }
+
+  getVideoId() {
+    let params = new URLSearchParams(location.search);
+    return params.get("v");
+  }
+
+  getShortVideoId() {
+    if (!this.isShorts()) {
+      return null;
+    }
+    return location.pathname.split("shorts/").pop();
+  }
+
+  // Boolean Methods
+
+  isSavedBlack() {
+    let videoId = this.getVideoId();
+    if (!videoId) {
+      return false;
+    }
+    return localStorage.getItem("tj::" + videoId);
+  }
+
+  isShorts() {
+    return location.pathname.includes("shorts/");
+  }
+
+  isShortSavedBlack() {
+    let videoId = this.getShortVideoId();
+    if (!videoId) {
+      return false;
+    }
+    return localStorage.getItem("tj::" + videoId);
+  }
+
+  // Methods
+
+  setViewedblack(id) {
+    localStorage.setItem("tj::" + id, document.title);
+  }
+
+  removeViewedBlack(id) {
+    localStorage.removeItem("tj::" + id);
+  }
+
+  createNotWatchedViewedBlackButton(id) {
+    return new ViewedBlackSwitchLargeButton().applyNotWatched(() => {
+      this.setViewedblack(id);
+      this.viewedBlack();
+    }).toElement();
+  }
+
+  createAlreadyWatchedViewedBlackButton(id) {
+    return new ViewedBlackSwitchLargeButton().applyAlready(() => {
+      this.removeViewedBlack(id);
+      this.viewedBlack();
+    }).toElement();
+  }
+
+  viewedBlack() {
+    if (this.getViewedBlackMain()) {
+      this.getViewedBlackMain().remove();
+    }
+    let below = this.getBelow();
+    if (!below) {
+      return;
+    }
+    below.removeAttribute("is-empty");
+    if (this.isSavedBlack()) {
+      below.prepend(this.createAlreadyWatchedViewedBlackButton(this.getVideoId()));
+    } else {
+      below.prepend(this.createNotWatchedViewedBlackButton(this.getVideoId()));
+    }
+    // shorts
+    if (this.isShorts()) {
+      if (this.isShortSavedBlack()) {
+        below.prepend(this.createAlreadyWatchedViewedBlackButton(this.getShortVideoId()));
+      } else {
+        below.prepend(this.createNotWatchedViewedBlackButton(this.getShortVideoId()));
+      }
+    }
+  }
+};
+
+class UrlChangeController {
+  constructor() {
+    this.oldUrl = "";
+    this.onUrlDidChangedToWatch = undefined;
+  }
+
+  registerOnUrlDidChangedToWatchListener(onUrlDidChangedToWatch) {
+    this.onUrlDidChangedToWatch = onUrlDidChangedToWatch;
+  }
+
+  onLoad() {
+    window.addEventListener("urlChange", this.urlChange.bind(this));
+  }
+
+  observe() {
+    if (this.oldUrl !== location.href) {
+      this.oldUrl = location.href;
+      window.dispatchEvent(new Event("urlChange"));
+    }
+  }
+
+  urlChange() {
+    if (location.href.includes("/watch?v=") && this.onUrlDidChangedToWatch) {
+      this.onUrlDidChangedToWatch();
+    }
+  }
 }
+
+class ContentScriptController {
+  constructor() {
+    this.oldUrl = "";
+    this.urlChangeController = new UrlChangeController();
+    this.showViewedBlackLargeButtonController = new ShowViewedBlackLargeButtonController();
+    this.deleteShortAreaController = new DeleteShortAreaController();
+    this.dismissAdController = new DismissAdController();
+  }
+
+  onLoad() {
+    window.addEventListener("load", this.setup.bind(this));
+    this.urlChangeController.registerOnUrlDidChangedToWatchListener(this.onUrlDidChangedToWatch.bind(this));
+    this.urlChangeController.onLoad();
+    this.dismissAdController.onLoad();
+    this.showViewedBlackLargeButtonController.onLoad();
+    this.deleteShortAreaController.onLoad();
+  }
+
+  setup() {
+    if (!document.body) {
+      window.setTimeout(this.setup.bind(this), 500);
+      return;
+    }
+    let observer = new MutationObserver((mutations) => {
+      this.urlChangeController.observe();
+      this.dismissAdController.observe();
+      this.showViewedBlackLargeButtonController.observe();
+      this.deleteShortAreaController.observe();
+    });
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+    let params = new URLSearchParams(location.search);
+    if (params.get("fullscreen")) {
+      document.documentElement.requestFullscreen();
+    }
+  }
+
+  onUrlDidChangedToWatch() {
+    this.showViewedBlackLargeButtonController.urlChange();
+    this.dismissAdController.urlChange();
+  }
+};
+
+((global) => {
+  "use strict";
+  new ContentScriptController().onLoad();
+})(this.self || global);
--- src/scripts/viewed_black.js
+++ src/scripts/viewed_black.js
@@ -1,70 +1,139 @@
-((global) => {
-  'use strict';
-  setup();
-})(this.self || global);
-
-var viewed_black_count = 0;
-var tj_switch_contrast = false;
-
-function setup() {
-  var target = document.querySelector('body');
-  if (!target) {
-    window.setTimeout(setup, 5000);
-    return;
+class VideoItem {
+  constructor(e) {
+    this.e = e;
   }
-  var observer = new MutationObserver((mutations) => {
-    if (viewed_black_count % 5 === 0) {
-      try {
-        chrome.storage.local.get({
-          tj_switch_contrast: false
-        }, (value) => {
-          tj_switch_contrast = value.tj_switch_contrast;
-          apply(value.tj_switch_contrast);
-        });
-      } catch (error) {
-        // console.error(error);
-      }
-    }
-    viewed_black_count++;
-  });
-  observer.observe(target, {
-    childList: true,
-    subtree: true
-  });
-}
 
-function apply(tj_switch_contrast) {
-  // ショート動画削除
-  [...document.querySelectorAll('div#contents ytd-rich-section-renderer')]
-    .filter((d) => !d.innerText.includes('新しい順'))
-    .forEach((d) => d.style.display = 'none');
-  
-  [...document.querySelectorAll('ytd-grid-video-renderer'), ...document.querySelectorAll('ytd-rich-item-renderer'), ...document.querySelectorAll('ytd-playlist-video-renderer')]
-  .forEach((e) => {
-    if (e.querySelector('a').href.split('&')[0].split('=')[1] && localStorage.getItem("tj::" + e.querySelector('a').href.split('&')[0].split('=')[1])) {
-      if (e.tagName.toLowerCase() === "ytd-playlist-video-renderer") {
-        if (e.querySelector('#progress')) {
-          e.style.opacity = "0.5";
-        } else {
-          e.style.opacity = "0.7";
-        }
-      } else {
-        e.style.opacity = "0.1";
-      }
-    } else if (e.querySelector('a').href.split('&')[0].split("shorts/")[1] && localStorage.getItem("tj::" + e.querySelector('a').href.split('&')[0].split("shorts/")[1])) {
-      if (e.tagName.toLowerCase() === "ytd-playlist-video-renderer") {
-        if (e.querySelector('#progress')) {
-          e.style.opacity = "0.5";
-        } else {
-          e.style.opacity = "0.7";
-        }
-      } else {
-        e.style.opacity = "0.1";
-      }
-    } else if (e.querySelector('#progress')) {
-      e.style.opacity = "0.5";
-    } else {
-      e.style.opacity = "1.0";
+  // Getter & Setter
+
+  get href() {
+    return this.e.querySelector('a')?.href;
+  }
+
+  get id() {
+    if (this.isShort()) {
+      return this.href.split("shorts/").pop();
     }
-  });
-}
+    return new URLSearchParams(new URL(this.href).search).get("v");
+  }
+
+  // Boolean Methods
+
+  isPlaylistVideo() {
+    return this.e.tagName.toLowerCase() === "ytd-playlist-video-renderer";
+  }
+
+  isProgress() {
+    return this.e.querySelector("#progress") !== null;
+  }
+
+  isShort() {
+    return this.href.includes("shorts/");
+  }
+
+  isViewedBlack() {
+    if (!this.id) {
+      return false;
+    }
+    return localStorage.getItem("tj::" + this.id) !== null;
+  }
+
+  // Opacity Methods
+
+  viewedBlackOpacity() {
+    if (this.isPlaylistVideo()) {
+      if (this.isProgress()) {
+        return "0.5";
+      }
+      return "0.7";
+    } else {
+      return "0.1";
+    }
+  }
+
+  progressOpacity() {
+    return "0.5";
+  }
+
+  resetOpacity() {
+    return "1.0";
+  }
+
+  // Apply Methods
+
+  applyViewedBlackOpacity() {
+    this.e.style.opacity = this.viewedBlackOpacity();
+  }
+
+  applyProgressOpacity() {
+    this.e.style.opacity = this.progressOpacity();
+  }
+
+  applyResetOpacity() {
+    this.e.style.opacity = this.resetOpacity();
+  }
+};
+
+class ViewedBlackController {
+  constructor() {
+    this.movieCount = 0;
+  }
+
+  onLoad() {
+    window.addEventListener("load", this.setup.bind(this));
+    window.addEventListener("movieCountChange", this.updateViewedBlackOpacity.bind(this));
+  }
+
+  setup() {
+    if (!document.body) {
+      window.setTimeout(this.setup, 5000);
+      return;
+    }
+    let observer = new MutationObserver((mutations) => {
+      if (this.movieCount !== this.getAllMovies().length) {
+        this.movieCount = this.getAllMovies().length;
+        window.dispatchEvent(new Event("movieCountChange"));
+      }
+    });
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  }
+
+  updateViewedBlackOpacity() {
+    try {
+      chrome.storage.local.get({ tj_switch_contrast: false }, (value) => {
+        this.switchContrast = value.tj_switch_contrast;
+        this.applyViewedBlackOpacity(value.tj_switch_contrast);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  applyViewedBlackOpacity() {
+    this.getAllMovies().forEach((e) => {
+      let videoItem = new VideoItem(e);
+      if (videoItem.isViewedBlack()) {
+        videoItem.applyViewedBlackOpacity();
+      } else if (videoItem.isProgress()) {
+        videoItem.applyProgressOpacity();
+      } else {
+        videoItem.applyResetOpacity();
+      }
+    });
+  }
+
+  getAllMovies() {
+    return [
+      ...document.querySelectorAll("ytd-grid-video-renderer"),
+      ...document.querySelectorAll("ytd-rich-item-renderer"),
+      ...document.querySelectorAll("ytd-playlist-video-renderer"),
+    ];
+  }
+};
+
+((global) => {
+  "use strict";
+  new ViewedBlackController().onLoad();
+})(this.self || global);
